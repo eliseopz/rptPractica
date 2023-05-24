5281c294c70026c0e892b2f402ca989827179a88
eliseopz <eliseo.paz15333@est.uca.edu.ni>
2023-05-24 15:52:31 -0600

    Practica ern clase

---
+++ ReportesPractica/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.BdHRDataSet = New ReportesPractica.BdHRDataSet()
+        Me.EmployeesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.EmployeesTableAdapter = New ReportesPractica.BdHRDataSetTableAdapters.employeesTableAdapter()
+        Me.JobsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.JobsTableAdapter = New ReportesPractica.BdHRDataSetTableAdapters.jobsTableAdapter()
+        CType(Me.BdHRDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.EmployeesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.JobsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
+        ReportDataSource1.Name = "DataSet1"
+        ReportDataSource1.Value = Me.JobsBindingSource
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
+        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "ReportesPractica.Report1.rdlc"
+        Me.ReportViewer1.Location = New System.Drawing.Point(0, 0)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.ServerReport.BearerToken = Nothing
+        Me.ReportViewer1.Size = New System.Drawing.Size(800, 450)
+        Me.ReportViewer1.TabIndex = 0
+        '
+        'BdHRDataSet
+        '
+        Me.BdHRDataSet.DataSetName = "BdHRDataSet"
+        Me.BdHRDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'EmployeesBindingSource
+        '
+        Me.EmployeesBindingSource.DataMember = "employees"
+        Me.EmployeesBindingSource.DataSource = Me.BdHRDataSet
+        '
+        'EmployeesTableAdapter
+        '
+        Me.EmployeesTableAdapter.ClearBeforeFill = True
+        '
+        'JobsBindingSource
+        '
+        Me.JobsBindingSource.DataMember = "jobs"
+        Me.JobsBindingSource.DataSource = Me.BdHRDataSet
+        '
+        'JobsTableAdapter
+        '
+        Me.JobsTableAdapter.ClearBeforeFill = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ReportViewer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.BdHRDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.EmployeesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.JobsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents BdHRDataSet As BdHRDataSet
+    Friend WithEvents EmployeesBindingSource As BindingSource
+    Friend WithEvents EmployeesTableAdapter As BdHRDataSetTableAdapters.employeesTableAdapter
+    Friend WithEvents JobsBindingSource As BindingSource
+    Friend WithEvents JobsTableAdapter As BdHRDataSetTableAdapters.jobsTableAdapter
+End Class
---
+++ ReportesPractica/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: esta línea de código carga datos en la tabla 'BdHRDataSet.jobs' Puede moverla o quitarla según sea necesario.
+        Me.JobsTableAdapter.Fill(Me.BdHRDataSet.jobs)
+        'TODO: esta línea de código carga datos en la tabla 'BdHRDataSet.employees' Puede moverla o quitarla según sea necesario.
+        Me.EmployeesTableAdapter.Fill(Me.BdHRDataSet.employees)
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+End Class
